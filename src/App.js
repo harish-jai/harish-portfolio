5efab12e1b8aeb360177ba9d27fabcc043a0a5f0
harish-jai <harish.jaisankar@gmail.com>
2025-02-17 17:28:29 -0500

    added favicon and title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Education from './pages/Education/Education';
@@ -11,6 +12,14 @@ import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <ThemeProvider>
+      <Helmet>
+        <title>Harish Jaisankar</title>
+        <meta name="description" content="A showcase of Harish's education, experience, projects, and leadership roles." />
+        <meta name="keywords" content="Harish, Jaisankar, Portfolio, Software Engineer, Web Developer, Intern" />
+        <meta name="author" content="Harish Jaisankar" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+      </Helmet>
+
       <Router>
         <Navbar />
         <Routes>
